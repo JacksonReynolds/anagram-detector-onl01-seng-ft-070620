b84014cd3b180aebc2de93f6bcf31dfd9c9846a5
Jackson Reynolds <jwreynold@gmail.com>
2020-07-14 18:18:01 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+
+  attr_reader :word
+
+  def initialize(word)
+    @word = word
+  end #initialize
+
+  def match(possible_anagrams)
+    possible_anagrams.find do |contender|
+      @word.sort == contender.sort
+    end #do
+
+end #Anagram
