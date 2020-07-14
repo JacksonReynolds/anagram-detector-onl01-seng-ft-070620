236ebcdd292de5e2b98d8cf035e47b3276400558
Jackson Reynolds <jwreynold@gmail.com>
2020-07-14 18:23:10 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+
+  attr_reader :word
+
+  def initialize(word)
+    @word = word
+  end #initialize
+
+  def match(possible_anagrams)
+    possible_anagrams.find_all do |contender|
+      @word.split('').sort.join == contender.split('').sort.join
+    end #do
+  end #match
+
+end #Anagram
